edf29d110bf20015cb0139a3f6c134223a77122d
Nailia Aleshicheva <gilmanova-nailya@mail.ru>
2025-05-02 21:03:13 +0300

    автотесты на мобилке (BrowserStack, Selene, Appium), Android и iOS

--- tests/mobile/test_wikipedia_android.py
+++ tests/mobile/test_wikipedia_android.py
@@ -6,7 +6,7 @@ from selene import browser, have
 def test_search(android_management):
 
     with step('Type search'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+#        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
 
@@ -18,14 +18,10 @@ def test_search(android_management):
 def test_open_article(android_management):
 
     with step('Type search'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+#        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
 
     with step('Open article'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.first.click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/closeButton')).click()
-        browser.element(
-            (AppiumBy.XPATH, '//android.widget.TextView[@text="Appium"]')
-        )
